1e66e7fd86995a0ec3fd1335b9e3d8eed3d933ce
ManuLosta <mlostalom@gmail.com>
2024-03-10 19:43:19 -0300

    some config

---
+++ server/src/main/java/com/example/server/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.server.repositories;
+
+import com.example.server.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String username);
+}
